3a66498175a4d691a270f36f3d77f22b0c3734b7
javabobr <semenmakusin06@gmail.com>
2026-01-27 01:41:47 +0700

    print_regnums

--- src/model_car.py
+++ src/model_car.py
@@ -2,6 +2,9 @@ import cv2
 import pytesseract as pytess
 from imutils import contours
 
+
+pytess.pytesseract.tesseract_cmd = r'"C:\Program Files\Tesseract-OCR"'
+
 image = cv2.imread("images/image99.jpg") #изображение авто
 
 height, width, _ = image.shape #высота, ширина и профиль цвета изображения
@@ -18,10 +21,20 @@ thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)[1]
 #выделяем контуры из изображения(знак - прямоугольный контур)
 cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0] #получаем именно список контуров
 cnts, _ = contours.sort_contours(cnts) #координаты крайних точек найденных контуров
-print(cnts) 
+
+
+for c in cnts:
+    area = cv2.contourArea(c)
+    x, y, w, h = cv2.boundingRect(c)
+    if area > 5000:
+        img = image[y:y+h, x:x+w]
+        result = pytess.image_to_string(img, lang="rus+eng")
+        if len(result) > 6:
+            print(result)
+
 
 #уменьшаем фото
-im = thresh
+im = image
 target_width = 500
 height, width = im.shape[:2]
 aspect_ratio = width / height
