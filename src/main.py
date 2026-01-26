47d7539ef569e55b451cd3d86141b6c1d101d120
javabobr <semenmakusin06@gmail.com>
2026-01-26 11:40:08 +0700

    1_plan

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 def main():
     print("Hello from ai-parking!")
 
-def popa():
-    print("POPAAAAa")
+# 1) обнаружение номерного знака - Object detection (YOLO, COCO - дообучить на датасете)
+# 2) обработка изображения номера - OpenCV
+# 3) распознавание символов - OCR - Tesseract
 if __name__ == "__main__":
     main()
