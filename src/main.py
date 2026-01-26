3d16d244aa62e577ead0791ace9609a247336e27
javabobr <semenmakusin06@gmail.com>
2026-01-27 00:21:32 +0700

    imports

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
-def main():
-    print("Hello from ai-parking!")
+import cv2
+import pytesseract as pytess
+
 
 # 1) обнаружение номерного знака - Object detection (YOLO, COCO - дообучить на датасете)
 # 2) обработка изображения номера - OpenCV
 # 3) распознавание символов - OCR - Tesseract
+
+def main():
+    print("Hello from ai-parking!")
+
 if __name__ == "__main__":
     main()
