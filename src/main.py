1e65d593d0fc08fe060caf3b2f64744554ccc390
javabobr <semenmakusin06@gmail.com>
2026-01-29 11:48:28 +0700

    sfgjfg

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import pytesseract as pytess
 
 # 1) обнаружение номерного знака - Object detection (YOLO, COCO - дообучить на датасете)
 # 2) обработка изображения номера - OpenCV
-# 3) распознавание символов - OCR - Tesseract
+# 3) распознавание символов - OCR 
 
 def main():
     print("Hello from ai-parking!")
