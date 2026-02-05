9e7d03b82c9250ed40b88295b4a589575da28278
whykunchik <89503032353v@gmail.com>
2026-02-06 01:53:26 +0700

    изменение шаблонов

--- ai_parking/settings.py
+++ ai_parking/settings.py
@@ -117,7 +117,7 @@ AUTHENTICATION_BACKENDS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Asia/Krasnoyarsk'
 
